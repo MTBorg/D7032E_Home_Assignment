0c5459c2e07120a0f11638c940afca194bc464f0
Martin Terneborg <martinterneborg@protonmail.com>
2019-10-17 16:02:27 +0200

    Extract message loop functionality in client

--- src/client/KingTokyoPowerUpClient.java
+++ src/client/KingTokyoPowerUpClient.java
@@ -7,15 +7,17 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.Scanner;
 
 public class KingTokyoPowerUpClient {
+  private final int SOCKET_HOST = 2048;
   private Scanner sc = new Scanner(System.in);
+  private boolean bot;
 
   public KingTokyoPowerUpClient(boolean bot) {
     String name = "";
-    Random rnd = ThreadLocalRandom.current();
+    this.bot = bot;
 
     //Server stuffs
     try {
-      Socket aSocket = new Socket("localhost", 2048);
+      Socket aSocket = new Socket("localhost", SOCKET_HOST);
       DataOutputStream outToServer = new DataOutputStream(
         aSocket.getOutputStream()
       );
@@ -25,41 +27,51 @@ public class KingTokyoPowerUpClient {
       name = inFromServer.readLine();
       System.out.println(name);
 
-      while (true) {
-        String[] message = inFromServer.readLine().split(":");
-        for (int i = 0; i < message.length; i++) {
-          System.out.println(message[i].toString());
-        }
-        if (message[0].equalsIgnoreCase("VICTORY")) {
-          outToServer.writeBytes("Bye!\n");
-        } else if (message[0].equalsIgnoreCase("ATTACKED")) {
-          if (bot) outToServer.writeBytes("YES\n"); else {
-            outToServer.writeBytes(sc.nextLine() + "\n");
-          }
-        } else if (message[0].equalsIgnoreCase("ROLLED")) {
-          if (bot) {
-            rnd = ThreadLocalRandom.current();
-            int num1 = rnd.nextInt(2) + 4;
-            int num2 = rnd.nextInt(2) + 1;
-            String reroll = "" + num1 + "," + num2 + "\n";
-            outToServer.writeBytes(reroll); // Some randomness at least
-          } else {
-            outToServer.writeBytes(sc.nextLine() + "\n");
-          }
-        } else if (message[0].equalsIgnoreCase("PURCHASE")) {
-          if (bot) outToServer.writeBytes("-1\n"); else outToServer.writeBytes(
-            sc.nextLine() + "\n"
-          );
-        } else {
-          if (bot) outToServer.writeBytes("OK\n"); else {
-            System.out.println("Press [ENTER]");
-            sc.nextLine();
-            outToServer.writeBytes("OK\n");
-          }
-        }
-        System.out.println("\n");
-      }
+      messageLoop(aSocket, outToServer, inFromServer);
     } catch (Exception e) {}
+  }
+
+  private void messageLoop(
+    Socket aSocket,
+    DataOutputStream outToServer,
+    BufferedReader inFromServer
+  )
+    throws Exception {
+    Random rnd = ThreadLocalRandom.current();
+    while (true) {
+      String[] message = inFromServer.readLine().split(":");
+      for (int i = 0; i < message.length; i++) {
+        System.out.println(message[i].toString());
+      }
+      if (message[0].equalsIgnoreCase("VICTORY")) {
+        outToServer.writeBytes("Bye!\n");
+      } else if (message[0].equalsIgnoreCase("ATTACKED")) {
+        if (bot) outToServer.writeBytes("YES\n"); else {
+          outToServer.writeBytes(sc.nextLine() + "\n");
+        }
+      } else if (message[0].equalsIgnoreCase("ROLLED")) {
+        if (bot) {
+          rnd = ThreadLocalRandom.current();
+          int num1 = rnd.nextInt(2) + 4;
+          int num2 = rnd.nextInt(2) + 1;
+          String reroll = "" + num1 + "," + num2 + "\n";
+          outToServer.writeBytes(reroll); // Some randomness at least
+        } else {
+          outToServer.writeBytes(sc.nextLine() + "\n");
+        }
+      } else if (message[0].equalsIgnoreCase("PURCHASE")) {
+        if (bot) outToServer.writeBytes("-1\n"); else outToServer.writeBytes(
+          sc.nextLine() + "\n"
+        );
+      } else {
+        if (bot) outToServer.writeBytes("OK\n"); else {
+          System.out.println("Press [ENTER]");
+          sc.nextLine();
+          outToServer.writeBytes("OK\n");
+        }
+      }
+      System.out.println("\n");
+    }
   }
 
   public static void main(String argv[]) {
