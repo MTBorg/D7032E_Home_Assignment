9acac6c1a452f11e7bdd3d68df10079ca575b633
MTBorg <martinterneborg@protonmail.com>
2019-10-23 09:05:23 +0200

    Broadcast message when store card is played

--- src/game/cards/store/DiscardCard.java
+++ src/game/cards/store/DiscardCard.java
@@ -3,6 +3,7 @@ package src.game.cards.store;
 import src.game.cards.store.StoreCard;
 import src.game.GameState;
 import src.game.Monster;
+import src.server.Server;
 
 abstract public class DiscardCard extends StoreCard {
 
@@ -12,6 +13,14 @@ abstract public class DiscardCard extends StoreCard {
 
   @Override
   public void execute(Monster executor, GameState gameState) {
+    Server.broadCastMessage(
+      executor.getName() + " played card " + this.name + "\n",
+      executor
+    );
+    Server.sendOneWayMessage(
+      executor.stream,
+      "You played card " + this.name + "\n"
+    );
     effect(executor, gameState);
     executor.removeCard(this);
 
