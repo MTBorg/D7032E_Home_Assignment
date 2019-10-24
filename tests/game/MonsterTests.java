754559acf55fdb6e8caa950b433a7be3b6a27a4e
MTBorg <martinterneborg@protonmail.com>
2019-10-24 11:06:23 +0200

    Add monster tests

--- tests/game/MonsterTests.java
+++ tests/game/MonsterTests.java
@@ -2,7 +2,12 @@ package tests.game;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import org.junit.Test;
+import src.game.Card;
+import src.game.cards.store.ArmorPlating;
+import src.game.cards.store.StoreCard;
+import src.game.GameState;
 import src.game.Monster;
 
 public class MonsterTests {
@@ -14,5 +19,49 @@ public class MonsterTests {
     monster.setCurrentHealth(monster.getMaxHealth() * 10);
 
     assertEquals(monster.getCurrentHealth(), monster.getMaxHealth());
+  }
+
+  @Test
+  public void giveCard() {
+    Monster monster = new Monster("Test Monster");
+    Card armorPlating = new ArmorPlating();
+    assertFalse(monster.hasCard(armorPlating.getName()));
+    monster.giveCard(armorPlating);
+    assertTrue(monster.hasCard(armorPlating.getName()));
+  }
+
+  @Test
+  public void removeCard() {
+    Monster monster = new Monster("Test Monster");
+    Card armorPlating = new ArmorPlating();
+    monster.giveCard(armorPlating);
+    assertTrue(monster.hasCard(armorPlating.getName()));
+    monster.removeCard(armorPlating);
+    assertFalse(monster.hasCard(armorPlating.getName()));
+  }
+
+  @Test
+  public void attackMonster() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    int damage = 3;
+    GameState gameState = new GameState(monsters);
+    assertEquals(monster.getCurrentHealth(), monster.getMaxHealth());
+    monster.attackMonster(monster, damage, gameState.eventQueue, gameState);
+    assertEquals(monster.getCurrentHealth(), monster.getMaxHealth() - damage);
+  }
+
+  @Test
+  public void buyCard() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    StoreCard armorPlating = new ArmorPlating();
+    monster.energy = armorPlating.cost;
+    GameState gameState = new GameState(monsters);
+    assertFalse(monster.hasCard(armorPlating.getName()));
+    monster.buyCard(gameState.eventQueue, armorPlating, gameState);
+    assertTrue(monster.hasCard(armorPlating.getName()));
   }
 }
