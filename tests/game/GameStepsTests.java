fd95b956249e9152e8699d9ce5ecfc99b3aa492b
MTBorg <martinterneborg@protonmail.com>
2019-10-26 16:44:15 +0200

    Add tests for victory point counting

--- tests/game/GameStepsTests.java
+++ tests/game/GameStepsTests.java
@@ -20,12 +20,24 @@ public class GameStepsTests {
    * Generate a dice roll (6 dice) with a specified amount of hearts and
    * the rest ones.
    */
-  private HashMap<Dice, Integer> generateDiceRoll(int hearts) {
+  private HashMap<Dice, Integer> generateDiceRollHearts(int hearts) {
     HashMap<Dice, Integer> result = new HashMap<Dice, Integer>();
     result.put(new Dice(0), hearts);
     result.put(new Dice(1), 6 - hearts);
     for (int i = 2; i < 6; i++) {
       result.put(new Dice(i), 0);
+    }
+    return result;
+  }
+
+  private HashMap<Dice, Integer> generateDiceRollNumber(
+    int number,
+    int amount
+  ) {
+    HashMap<Dice, Integer> result = new HashMap<Dice, Integer>();
+    result.put(new Dice(0), 6 - amount);
+    for (int i = 1; i < 6; i++) {
+      result.put(new Dice(i), (i == number) ? amount : 0);
     }
     return result;
   }
@@ -36,7 +48,7 @@ public class GameStepsTests {
     ArrayList<Monster> monsters = new ArrayList<Monster>();
     monsters.add(monster);
     GameState gameState = new GameState(monsters);
-    HashMap<Dice, Integer> diceRoll = generateDiceRoll(1);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollHearts(1);
     monster.setCurrentHealth(1);
 
     assertEquals(monster.getCurrentHealth(), 1);
@@ -50,7 +62,7 @@ public class GameStepsTests {
     ArrayList<Monster> monsters = new ArrayList<Monster>();
     monsters.add(monster);
     GameState gameState = new GameState(monsters);
-    HashMap<Dice, Integer> diceRoll = generateDiceRoll(0);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollHearts(0);
     monster.setCurrentHealth(1);
 
     assertEquals(monster.getCurrentHealth(), 1);
@@ -64,7 +76,7 @@ public class GameStepsTests {
     ArrayList<Monster> monsters = new ArrayList<Monster>();
     monsters.add(monster);
     GameState gameState = new GameState(monsters);
-    HashMap<Dice, Integer> diceRoll = generateDiceRoll(3);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollHearts(3);
 
     assertEquals(monster.getCurrentHealth(), monster.getMaxHealth());
     GameSteps.countHearts(monster, diceRoll, gameState);
@@ -78,7 +90,7 @@ public class GameStepsTests {
     ArrayList<Monster> monsters = new ArrayList<Monster>();
     monsters.add(monster);
     GameState gameState = new GameState(monsters);
-    HashMap<Dice, Integer> diceRoll = generateDiceRoll(1);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollHearts(1);
     monster.setCurrentHealth(1);
 
     //TODO: This feature is not implemented
@@ -93,7 +105,7 @@ public class GameStepsTests {
     ArrayList<Monster> monsters = new ArrayList<Monster>();
     monsters.add(monster);
     GameState gameState = new GameState(monsters);
-    HashMap<Dice, Integer> diceRoll = generateDiceRoll(3);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollHearts(3);
 
     //TODO: This is currently not possible to test
     assertEquals(monster.cardsToString(), "[NO CARDS]");
@@ -129,5 +141,161 @@ public class GameStepsTests {
     assertEquals(monster.stars, 0);
     GameSteps.checkIfInTokyo(monster);
     assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsZeroOnes() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(1, 0);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsTwoOnes() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(1, 2);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsThreeOnes() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(1, 3);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 1);
+  }
+
+  @Test
+  public void countVictoryPointsFourOnes() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(1, 4);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 2);
+  }
+
+  @Test
+  public void countVictoryPointsZeroTwos() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(2, 0);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsTwoTwos() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(2, 2);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsThreeTwos() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(2, 3);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 2);
+  }
+
+  @Test
+  public void countVictoryPointsFourTwos() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(2, 4);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 3);
+  }
+
+  @Test
+  public void countVictoryPointsZeroThrees() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(3, 0);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsTwoThrees() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(3, 2);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 0);
+  }
+
+  @Test
+  public void countVictoryPointsThreeThrees() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(3, 3);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 3);
+  }
+
+  @Test
+  public void countVictoryPointsFourThrees() {
+    Monster monster = new Monster("Test Monster");
+    ArrayList<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(monster);
+    GameState gameState = new GameState(monsters);
+    HashMap<Dice, Integer> diceRoll = generateDiceRollNumber(3, 4);
+
+    assertEquals(monster.stars, 0);
+    GameSteps.countVictoryPoints(monster, diceRoll);
+    assertEquals(monster.stars, 4);
   }
 }
